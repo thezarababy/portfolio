bf5ec7770c814925b756fbf23d6302c835bbb509
thezarababy <jyokoduwa@gmail.com>
2025-07-24 07:24:13 +0100

    hero

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -8,17 +8,17 @@ const Hero = () => {
     >
       <main className="flex  flex-col text-slate-700 max-md:text-center ">
         <p className="text-2xl max-md:text-lg ">Hey, i'm Joy👋</p>
-        <h1 className=" font-extrabold mt-4 text-5xl  text-slate-700 max-md:text-2xl font-bold ">
+        <h1 className=" font-extrabold mt-4 text-5xl  text-slate-700 max-md:text-2xl font-bold max-md:w-full">
           {" "}
           <span className="text-blue-900 ">Full</span>Stack Developer
         </h1>
-        <p className="text-2xl w-2/3  max-md:mx-auto pt-2 text-lg w-full">
+        <p className="text-2xl  w-[60%] max-md:mx-auto pt-2 max-md:text-lg max-md:w-[70%] ">
           Passionate Full Stack Developer building seamless digital experiences
         </p>
-        <div className="mt-6 max-w-[500px] max-md:flex flex-col max-w-[200px] mx-auto   ">
+        <div className="mt-6 max-w-[500px] ml-2  max-md:flex flex-col max-md:mx-auto  ">
           <a
             href="#contact"
-            className="bg-blue-900 px-5 py-3 rounded-lg text-white mr-4  max-md:mb-4 ml-2 text-sm"
+            className="bg-blue-900 px-5 py-3 rounded-lg text-white mr-4  max-md:mb-4 ml-2 max-md:text-sm"
           >
             {" "}
             Get In Touch
