e7be252485250c9e5ab8b7223727e7755e835e60
thezarababy <jyokoduwa@gmail.com>
2025-07-24 06:59:42 +0100

    header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,29 +3,25 @@ import { RxCross1, RxHamburgerMenu } from "react-icons/rx";
 import Sidebar from "./Sidebar";
 
 const Header = () => {
-  const [toggle, setToggle] = useState("false");
+  const [toggle, setToggle] = useState(false);
   const handleToggle = () => {
     setToggle(!toggle);
   };
   return (
-    <div id="header" className="max-w-[1440px] mx-auto  ">
-      {" "}
-      <div className="fixed top-0 w-full bg-white shadow z-50 flex justify-between items-center p-7 text-slate-700">
+    <div
+      id="header"
+      className="fixed top-0 w-full bg-white shadow z-50 p-7 text-slate-700"
+    >
+      <div className="flex justify-between items-center max-w-7xl mx-auto">
+        {/* Logo */}
         <div>
-          <h1 className="text-3xl font-semibold">
-            {" "}
+          <h1 className="text-3xl font-semibold max-md:text-2xl">
             my<span className="text-blue-900">Portfolio</span>
           </h1>
         </div>
-        <div className="hidden max-md:block">
-          {toggle ? (
-            <RxCross1 size={30} onClick={handleToggle} />
-          ) : (
-            <RxHamburgerMenu size={30} onClick={handleToggle} />
-          )}
-        </div>
-        <div className="max-md:hidden">
-          <ul className="flex gap-6   font-semibold text-2xl">
+        {/* Desktop Navigation */}
+        <nav className="max-md:hidden">
+          <ul className="flex gap-6 font-semibold text-2xl">
             <li>
               <a href="#hero" className="hover:text-purple-800">
                 Home
@@ -52,9 +48,19 @@ const Header = () => {
               </a>
             </li>
           </ul>
+        </nav>
+
+        {/* Mobile Menu Toggle */}
+        <div className="hidden max-md:block p-3">
+          {toggle ? (
+            <RxCross1 size={30} onClick={handleToggle} />
+          ) : (
+            <RxHamburgerMenu size={30} onClick={handleToggle} />
+          )}
         </div>
       </div>
-      <div>{toggle && <Sidebar handleToggle={handleToggle} />}</div>
+      {/* Sidebar for Mobile */}
+      {toggle && <Sidebar handleToggle={handleToggle} />}
     </div>
   );
 };
